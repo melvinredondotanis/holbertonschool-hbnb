09f3d42fd596080567e3a0719b12c136f801e745
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-10-21 14:23:24 +0200

    Refactor user creation endpoint in users.py and run.py

--- part2/hbnb/run.py
+++ part2/hbnb/run.py
@@ -1,9 +1,8 @@
 from app import create_app
-from config import DevelopmentConfig
 
 
 app = create_app()
 
 
 if __name__ == '__main__':
-    app.run(debug=DevelopmentConfig.DEBUG)
+    app.run(debug=True)
