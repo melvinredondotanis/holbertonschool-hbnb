d4e0c8f3705dfc081429f6adf9c4f2bccda4194a
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-10-21 14:51:21 +0200

    Refactor facade.py to add support for managing places

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,6 +1,7 @@
-from app.persistence.repository import InMemoryRepository
 from app.models.user import User
+from app.models.place import Place
 from app.models.amenity import Amenity
+from app.persistence.repository import InMemoryRepository
 
 
 class HBnBFacade:
@@ -9,6 +10,7 @@ class HBnBFacade:
         self.place_repo = InMemoryRepository()
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
+
 
     def create_user(self, user_data):
         user = User(**user_data)
@@ -21,30 +23,42 @@ class HBnBFacade:
     def get_user_by_email(self, email):
         return self.user_repo.get_by_attribute('email', email)
 
-    # Placeholder method for fetching a place by ID
-    def get_place(self, place_id):
-        # Logic will be implemented in later tasks
-        pass
 
     def create_amenity(self, amenity_data):
-        # Create new amenity
         new_amenity = Amenity(**amenity_data)
         return self.amenity_repo.add(new_amenity)
     
     def get_amenity(self, amenity_id):
-        # Retrieve an amenity by ID
         return self.amenity_repo.get(amenity_id)
 
     def get_all_amenities(self):
-        # Retrieve all amenities
         return self.amenity_repo.get_all()
 
     def update_amenity(self, amenity_id, amenity_data):
-        # Update an existing amenity
         amenity = self.amenity_repo.get(amenity_id)
         if amenity:
             for key, value in amenity_data.items():
                 setattr(amenity, key, value)
             return self.amenity_repo.update(amenity)
         return None
-    
+
+
+    def create_place(self, place_data):
+        place = Place(**place_data)
+        self.place_repo.add(place)
+        return place
+
+    def get_place(self, place_id):
+        return self.place_repo.get(place_id)
+
+    def get_all_places(self):
+        return self.place_repo.get_all()
+
+    def update_place(self, place_id, place_data):
+        place = self.place_repo.get(place_id)
+        if place:
+            for key, value in place_data.items():
+                setattr(place, key, value)
+            return self.place_repo.update(place)
+        return None
+        
