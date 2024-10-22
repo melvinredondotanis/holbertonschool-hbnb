a983455d6f3024b543a9d577be929e718c59c22a
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-10-22 09:08:08 +0200

    Refactor HBnBFacade import in amenities.py and users.py to use app.services.facade

--- part2/hbnb/app/api/v1/amenities.py
+++ part2/hbnb/app/api/v1/amenities.py
@@ -1,5 +1,6 @@
 from flask_restx import Namespace, Resource, fields
-from app.services.facade import HBnBFacade
+from app.services import facade
+
 
 api = Namespace('amenities', description='Amenity operations')
 
@@ -8,7 +9,6 @@ amenity_model = api.model('Amenity', {
     'name': fields.String(required=True, description='Name of the amenity')
 })
 
-facade = HBnBFacade()
 
 @api.route('/')
 class AmenityList(Resource):
