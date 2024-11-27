5857cfa493d92a42934efb021823b834af2dbead
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-11-27 16:41:00 +0100

    feat: implement CORS support, add default admin and amenities to database, and enhance front-end with favicon and improved login functionality

--- part4/hbnb/back/app/__init__.py
+++ part4/hbnb/back/app/__init__.py
@@ -3,6 +3,7 @@ from flask_restx import Api
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 # Create here to avoid circular imports
 bcrypt = Bcrypt()
@@ -15,6 +16,8 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
+from app.models.user import User
+from app.models.amenity import Amenity
 
 
 def create_app(config_class=config.DevelopmentConfig):
@@ -22,6 +25,7 @@ def create_app(config_class=config.DevelopmentConfig):
     Create and configure the Flask application.
     """
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object(config_class)
     api = Api(
         app,
@@ -52,5 +56,42 @@ def create_app(config_class=config.DevelopmentConfig):
 
     with app.app_context():
         db.create_all()
+        admin = User.query.filter_by(email='admin@hbnb.com').first()
+        if not admin:
+            admin = User(
+                id='36c9050e-ddd3-4c3b-9731-9f487208bbc1',
+                _first_name='Admin',
+                _last_name='HBnB',
+                _email='admin@hbnb.io',
+                _password='$2b$12$yPzwR02tJub1wfuWcCoZ8eL8aK6a2nz5SAeU9KVS7m//K48OuemVW',
+                _is_admin=True
+            )
+            db.session.merge(admin)
+
+            wifi = Amenity.query.filter_by(id='ad6cbba4-33c9-48e3-b6ee-d494ee5a6b45').first()
+            if not wifi:
+                wifi = Amenity(
+                id='ad6cbba4-33c9-48e3-b6ee-d494ee5a6b45',
+                _name='WiFi'
+                )
+                db.session.merge(wifi)
+
+            pool = Amenity.query.filter_by(id='ad6cbba4-33c9-48e3-b6ee-d494ee5a6b46').first()
+            if not pool:
+                pool = Amenity(
+                id='ad6cbba4-33c9-48e3-b6ee-d494ee5a6b46',
+                _name='Swimming Pool'
+                )
+                db.session.merge(pool)
+
+            ac = Amenity.query.filter_by(id='0b8744c5-5c58-4c4a-8467-48392e306e28').first()
+            if not ac:
+                ac = Amenity(
+                id='0b8744c5-5c58-4c4a-8467-48392e306e28',
+                _name='Air Conditioning'
+                )
+                db.session.merge(ac)
+                
+            db.session.commit()
 
     return app
--- part4/hbnb/front/scripts.js
+++ part4/hbnb/front/scripts.js
@@ -1,8 +1,55 @@
-/* 
-  This is a SAMPLE FILE to get you started.
-  Please, follow the project instructions to complete the tasks.
-*/
+// Initialize the API URL
+const host = 'localhost';
+const port = '5000';
+
+
+// API URL for the login endpoint
+const login = 'http://' + host + ':' + port + '/api/v1/auth/login';
+async function login_user(email, password)
+{
+  const login_data = {
+    'email': email,
+    'password': password
+  };
+
+  const response = await fetch(login, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(login_data)
+  });
+
+  return response;
+}
+
 
 document.addEventListener('DOMContentLoaded', () => {
-    /* DO SOMETHING */
-  });
+  const loginForm = document.getElementById('login-form');
+
+  if (loginForm)
+  {
+    loginForm.addEventListener('submit', async (event) => {
+      event.preventDefault();
+      
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
+      
+      if (!email || !password)
+      {
+        alert('Email and password are required');
+        return;
+      }
+
+      const response = await login_user(email, password);
+      if (response.ok)
+      {
+        const data = await response.json();
+        document.cookie = `token=${data.access_token}; path=/`;
+        window.location.href = 'index.html';
+      }
+      else
+        alert('Login failed: ' + response.statusText);
+    });
+  }
+});
